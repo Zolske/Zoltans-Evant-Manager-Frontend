234e56e0e8cf11e3ad3b429914173285cc0a5524
Zoltan Kepes <zoltan.the.kepes@gmail.com>
2025-05-20 18:27:50 +0100

    reset project

--- app/src/main/java/com/example/zoltanseventmanagerfrontend/MainActivity.kt
+++ app/src/main/java/com/example/zoltanseventmanagerfrontend/MainActivity.kt
@@ -1,36 +1,29 @@
 package com.example.zoltanseventmanagerfrontend
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.zoltanseventmanagerfrontend.ui.GoogleSignInUtils
-import com.example.zoltanseventmanagerfrontend.ui.theme.LogInTheme
+import com.example.zoltanseventmanagerfrontend.ui.theme.ZoltansEventManagerFrontendTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            LogInTheme {
+            ZoltansEventManagerFrontendTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MainScreen(modifier = Modifier.padding(innerPadding))
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
                 }
             }
         }
@@ -38,38 +31,17 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MainScreen(modifier: Modifier = Modifier) {
-    val context = LocalContext.current
-    val scope = rememberCoroutineScope()
-    val launcher = rememberLauncherForActivityResult(contract = ActivityResultContracts.StartActivityForResult()) {
-        GoogleSignInUtils.doGoogleSignIn(
-            context = context,
-            scope = scope,
-            launcher = null,
-            login = {
-                Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
-            }
-        )
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
 
-    }
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-
-        Button(onClick = {
-            GoogleSignInUtils.doGoogleSignIn(
-                context = context,
-                scope = scope,
-                launcher = launcher,
-                login = {
-                    Toast.makeText(context, "Login successful", Toast.LENGTH_SHORT).show()
-                }
-            )
-        }) {
-            Text(text = "Google Sign in")
-        }
-
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ZoltansEventManagerFrontendTheme {
+        Greeting("Android")
     }
 }
